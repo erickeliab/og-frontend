ec6e1938b6b244e7315ccbbc3e9edb4750c557e5
erickmgongo <erickemmanuel661@gmail.com>
2021-08-04 17:12:30 +0300

    backend url

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ export const server = dev ? 'http://localhost:3000' : 'https://og-backend-m4e47.
 
 export const localbackend = 'http://erick:1337';
 
-export const productionbackend = 'https://theogbackend.herokuapp.com';
+export const productionbackend = 'https://og-backend-m4e47.ondigitalocean.app';
 
-export const backend = localbackend;
+export const backend = productionbackend;
 
