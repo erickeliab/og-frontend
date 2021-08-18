12805ab00636d46d8cc7ed017727f1bb547b2b66
erickmgongo <erickemmanuel661@gmail.com>
2021-08-18 17:41:53 +0300

    videos link

--- components/videos/PostItem.js
+++ components/videos/PostItem.js
@@ -6,7 +6,7 @@ import articleStyles from '../../styles/Article.module.css'
 const PostItem = ({ post }) => {
 	let content = '';
 
-	if (post) content =  <Link href={`/post/${post.id}`}>
+	if (post) content =  <Link href={{ pathname: '/viewpost', query: { keyword: post.id } }}>
 	<div class="col-md-3 d-flex ftco-animat">
 			  <div class="blog-entry justify-content-end">
 			  <div class="text text-center card">
